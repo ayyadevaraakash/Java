4a6c845931faf49120626a3a35d99277e75f845e
akash <akashudsouza12@gmail.com>
2022-09-11 17:21:32 +0530

    changes

--- Basic/LargestElement.java
+++ Basic/LargestElement.java
@@ -2,23 +2,21 @@ import java.util.Scanner;
 
 public class LargestElement {
   public static void main(String[] args) {
-    try (Scanner sc = new Scanner(System.in)) {
-      System.out.print("Enter number of elements: ");
-      int i;
-      int numberOfElements = sc.nextInt();
-      int[] myArray = new int[numberOfElements];
-      // take input
-      System.out.print("Enter elements: ");
-      for (i = 0; i < numberOfElements; i++) {
-        myArray[i] = sc.nextInt();
-      }
-      // logic
-      int max = myArray[0];
-      for (i = 1; i < numberOfElements; i++) {
-        max = (max > myArray[i]) ? max : myArray[i];
-      }
-      System.out.print("Max Element: " + max);
-      sc.close();
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number of elements: ");
+    int numberOfElements = sc.nextInt();
+    int[] myArray = new int[numberOfElements];
+    // take input
+    System.out.print("Enter elements: ");
+    for (int i = 0; i < numberOfElements; i++) {
+      myArray[i] = sc.nextInt();
     }
+    // logic
+    int max = myArray[0];
+    for (int i = 1; i < numberOfElements; i++) {
+      max = (max > myArray[i]) ? max : myArray[i];
+    }
+    System.out.print("Max Element: " + max);
+    sc.close();
   }
 }

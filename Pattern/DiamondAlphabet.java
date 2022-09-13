e066085e6cc2b08e0e3c83f3602851c1cbc2f0b0
akash <akashudsouza12@gmail.com>
2022-09-13 22:21:37 +0530

    efficient diamond

--- Pattern/DiamondAlphabet.java
+++ Pattern/DiamondAlphabet.java
@@ -1,5 +1,5 @@
 public class DiamondAlphabet {
-  public static void main(String[] args) {
+  public static void main(String args[]) {
     int n = 6;
     char ch = 65;
     for (int i = 0; i < n; i++) {
@@ -7,38 +7,26 @@ public class DiamondAlphabet {
         System.out.print(" ");
       }
       for (int k = 0; k <= i; k++) {
-        if (k == 0)
-          System.out.print(ch);
+        if (k == 0 || k == i)
+          System.out.print(ch + " ");
         else
-          System.out.print(" ");
-      }
-      for (int k = 0; k < i; k++) {
-        if (k == i - 1)
-          System.out.print(ch);
-        else
-          System.out.print(" ");
+          System.out.print("  ");
       }
       ch++;
       System.out.println();
     }
-    ch -= 2;
-    for (int i = 1; i < n; i++) {
-      for (int j = 0; j <= i; j++) {
+    ch--;
+    for (int i = 0; i < n - 1; i++) {
+      ch--;
+      for (int j = 0; j <= i + 1; j++) {
         System.out.print(" ");
       }
-      for (int k = 0; k < n - i; k++) {
-        if (k == 0)
-          System.out.print(ch);
-        else
-          System.out.print(" ");
-      }
       for (int k = 0; k < n - i - 1; k++) {
-        if (k == n - i - 2)
-          System.out.print(ch);
+        if (k == 0 || k == n - i - 2)
+          System.out.print(ch + " ");
         else
-          System.out.print(" ");
+          System.out.print("  ");
       }
-      ch--;
       System.out.println();
     }
   }

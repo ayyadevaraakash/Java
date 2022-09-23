93669d984a5428aaadb7b27b8ab6cff884d7afe8
unknown <akashudsouza12@gmail.com>
2022-09-23 23:15:56 +0530

    ac problems

---
+++ Problems/PowerOfTwo.java
@@ -0,0 +1,16 @@
+// Program to check whether the given input is power of 2
+
+import java.util.Scanner;
+
+class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any positive number: ");
+        int inputNumber = sc.nextInt();
+        if ((inputNumber & (inputNumber - 1)) == 0 && inputNumber != 0)
+            System.out.println("Power of 2");
+        else
+            System.out.println("Not a power of 2");
+        sc.close();
+    }
+}

f0729bd5ce0f31b8567b1ab09fe6259f43dcbed2
unknown <akashudsouza12@gmail.com>
2022-09-24 00:49:45 +0530

    medium leetcode AC suggested

---
+++ Problems/CyclicRotation.java
@@ -0,0 +1,28 @@
+// Given a number n < length of array. Do cyclic rotation of that many elements from end
+
+import java.util.Arrays;
+
+class CyclicRotation {
+
+    static void reverse(int[] arr, int low, int high) {
+        int mid = (high - low + 1) / 2;
+        int m = low, n = high, temp;
+        for (int i = 0; i < mid; i++, m++, n--) {
+            temp = arr[m];
+            arr[m] = arr[n];
+            arr[n] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        int[] arr = { 12, 4, 9, 11, 5, 17 };
+        int partition = arr.length - (n + 1);
+
+        reverse(arr, 0, partition);
+        reverse(arr, partition + 1, arr.length - 1);
+        reverse(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}

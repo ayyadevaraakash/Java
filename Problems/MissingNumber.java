1edaae5c9e4e2238e252264774957dec93b2e922
akashinfinity12 <akashudsouza12@gmail.com>
2022-09-25 00:11:27 +0530

    missing number using bitwise

---
+++ Problems/MissingNumber.java
@@ -0,0 +1,14 @@
+// Find the missing number in the sequence from 0 to n
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 5, 6, 7, 8, 9 };
+        int val = arr[0];
+        for (int i = 1; i < arr.length; i++)
+            val ^= arr[i];
+        for (int i = 1; i < arr[arr.length - 1] + 1; i++) {
+            val ^= i;
+        }
+        System.out.println("Missing number is " + val);
+    }
+}

a8f2d643454de98ec24e61582876b2a55c0b6010
akash <akashudsouza12@gmail.com>
2022-10-23 12:28:21 +0530

    leetcode search matrix

---
+++ Problems/SearchMatrix.java
@@ -0,0 +1,31 @@
+public class SearchMatrix {
+  public static void main(String[] args) {
+    int[][] matrix = {
+        { 1, 3, 5, 7 },
+        { 10, 11, 16, 20 },
+        { 23, 30, 34, 60 }
+    };
+
+    int target = 16;
+    int numberOfColumns = matrix[0].length;
+    int targetRow = 0;
+    boolean flag = false;
+
+    for (int i = 0; i < matrix.length; i++) {
+      if (matrix[i][numberOfColumns - 1] >= target) {
+        targetRow = i;
+        break;
+      }
+    }
+
+    for (int i = 0; i < matrix[0].length; i++) {
+      if (matrix[targetRow][i] == target) {
+        flag = true;
+        break;
+      }
+    }
+
+    System.out.println("Element is present: " + flag);
+
+  }
+}

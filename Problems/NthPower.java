dc1be4fa4cbfffffeb22901908fb2cc1b879a52b
akash <akashudsouza12@gmail.com>
2022-11-06 18:07:43 +0530

    partially done

---
+++ Problems/NthPower.java
@@ -0,0 +1,32 @@
+// Problem: Calculate nth root of a number
+
+class NthPower {
+
+  static int multiplier(int a, int b) {
+    int answer = a;
+    for (int i = 1; i < b; i++) {
+      answer = answer * a;
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    int number = 27, root = 3, low = 1;
+    int high = number, answer = -1;
+
+    while (high > low) {
+      int mid = (low + high) / 2;
+      int res = multiplier(mid, root);
+      if (res == number) {
+        answer = res;
+        break;
+      } else if (res < mid)
+        high = mid;
+      else
+        low = mid;
+    }
+
+    System.out.println("Result: " + answer);
+
+  }
+}

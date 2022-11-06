2d6d576aca001fbc43c19eebc64d0d52198fa3f2
akashinfinity12 <akashudsouza12@gmail.com>
2022-11-07 00:19:28 +0530

    partially done

---
+++ Problems/MinimizeHeights.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class MinimizeHeights {
+    public static void main(String[] args) {
+        int arr[] = { 3, 9, 12, 16, 20 };
+        int length = arr.length;
+        Arrays.sort(arr);
+        int k = 3;
+
+        int difference = arr[length - 1] - arr[0];
+        int min, max;
+
+        for (int i = 0; i < length - 1; i++) {
+            max = Math.max(arr[i] + k, arr[length - 1] - k);
+            min = Math.min(arr[i + 1] - k, arr[0] + k);
+
+            if (min < 0)
+                continue;
+            else
+                difference = Math.min(difference, max - min);
+        }
+
+        System.out.println("Minimum distance is: " + difference);
+
+    }
+}

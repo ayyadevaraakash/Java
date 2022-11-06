58443c253f75017b32959dfc29cd869917ae1b6a
akashinfinity12 <akashudsouza12@gmail.com>
2022-11-07 00:24:45 +0530

    dsa array hard

--- Problems/MinimizeHeights.java
+++ Problems/MinimizeHeights.java
@@ -1,3 +1,5 @@
+// Problem: https://practice.geeksforgeeks.org/problems/minimize-the-heights3351/1
+
 import java.util.Arrays;
 
 class MinimizeHeights {
@@ -24,3 +26,11 @@ class MinimizeHeights {
 
     }
 }
+
+// Logic
+
+/*
+ * Sort the array. Because after sorting the minimum heights can be obtained
+ * with adjacent elements.
+ * Kindly watch some good youtube video for the visual explanation.
+ */

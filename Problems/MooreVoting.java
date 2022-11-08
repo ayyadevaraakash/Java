595f68bf0705428292c0adc7b797053c5fcd91d2
akash <akashudsouza12@gmail.com>
2022-11-08 19:34:09 +0530

    Moore Algorithm

---
+++ Problems/MooreVoting.java
@@ -0,0 +1,8 @@
+// Problem: https://leetcode.com/problems/majority-element/
+
+public class MooreVoting {
+  public static void main(String[] args) {
+    int arr[] = { 2, 3, 5, 3, 3 };
+    int length = arr.length;
+  }
+}

63c8dad50d793090f9034f8cbb51cf9c1037143c
akash <akashudsouza12@gmail.com>
2022-11-08 19:41:44 +0530

    Striver's SDE sheet

--- Problems/MooreVoting.java
+++ Problems/MooreVoting.java
@@ -3,6 +3,23 @@
 public class MooreVoting {
   public static void main(String[] args) {
     int arr[] = { 2, 3, 5, 3, 3 };
-    int length = arr.length;
+    int majorityElement = arr[0], count = 0;
+
+    for (int i : arr) {
+      if (count == 0)
+        majorityElement = i;
+      if (i == majorityElement)
+        count++;
+      else
+        count--;
+    }
+
+    System.out.println("Majority Element: " + majorityElement);
   }
 }
+
+// Logic
+
+/*
+ * Solved using Moore's Voting Algorithm
+ */

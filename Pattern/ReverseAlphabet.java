55c519cccf951ad08dc99746634ca841edddbb46
akash <akashudsouza12@gmail.com>
2022-11-09 20:02:22 +0530

    less loc

--- Pattern/ReverseAlphabet.java
+++ Pattern/ReverseAlphabet.java
@@ -3,6 +3,13 @@
 
 class ReverseAlphabet {
   public static void main(String[] args) {
-
+    int n = 5;
+    int alphabet = 65;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= i; j++) {
+        System.out.print((char) (alphabet + 4 - i + j) + " ");
+      }
+      System.out.println();
+    }
   }
 }

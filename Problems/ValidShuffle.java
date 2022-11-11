b8592203345e6297cc01c6282b936c271934b7f0
akash <akashudsouza12@gmail.com>
2022-11-11 15:11:42 +0530

    partial done, time up

--- Problems/ValidShuffle.java
+++ Problems/ValidShuffle.java
@@ -1,7 +1,33 @@
+import java.util.Arrays;
+
 // Problem: https://www.codingninjas.com/codestudio/problems/shuffle-two-strings_1115692?topList=love-babbar-dsa-sheet-problems&utm_source=website&utm_medium=affiliate&utm_campaign=450dsatracker
 
 public class ValidShuffle {
-  public static void main(String[] args) {
 
+  static boolean checkValidShuffle(String t1, String t2, String test) {
+    int length1 = t1.length();
+    int length2 = t2.length();
+    int resultLength = test.length();
+
+    char[] temp = t1.toCharArray();
+    Arrays.sort(temp);
+    t1 = temp.toString();
+
+    if (length1 + length2 == resultLength) {
+
+      return true;
+    } else
+      return false;
+  }
+
+  public static void main(String[] args) {
+    String text1 = "XYZ";
+    String text2 = "123";
+    String shuffleText = "X12Y3Z";
+
+    if (checkValidShuffle(text1, text2, shuffleText))
+      System.out.println("Valid Shuffle");
+    else
+      System.out.println("Not a valid shuffle");
   }
 }

8968419ce6eae81d960ef28389cccd2d3aa087a7
akash <akashudsouza12@gmail.com>
2022-11-18 14:36:13 +0530

    optimized version

--- Problems/MobileKeypad.java
+++ Problems/MobileKeypad.java
@@ -1,40 +1,17 @@
-import java.util.Arrays;
-import java.util.HashMap;
-
 class MobileKeypad {
   public static void main(String[] args) {
-    HashMap<String, Character> keypad = new HashMap<>();
-    keypad.put("ABC", '2');
-    keypad.put("DEF", '3');
-    keypad.put("GHI", '4');
-    keypad.put("JKL", '5');
-    keypad.put("MNO", '6');
-    keypad.put("PQRS", '7');
-    keypad.put("TUV", '8');
-    keypad.put("WXYZ", '9');
+    String str[] = { "2", "22", "222", "3", "33", "333",
+        "4", "44", "444", "5", "55", "555",
+        "6", "66", "666", "7", "77", "777",
+        "7777", "8", "88", "888", "9", "99",
+        "999", "9999" };
 
-    char[] sameCharacters = { '1', '0', '*', '#', ' ' };
-    String inputSentence = "GEEKSFORGEEKS";
-    int length = inputSentence.length();
-    System.out.print("Numeric Sequence: ");
+    String inputText = "AADITYA AKASH AMAN";
+    int length = inputText.length();
 
     for (int i = 0; i < length; i++) {
-      if (Arrays.binarySearch(sameCharacters, inputSentence.charAt(i)) >= 0)
-        System.out.print(inputSentence.charAt(i));
-      else {
-        int copy = i;
-        keypad.forEach((k, v) -> {
-          if (k.contains(String.valueOf(inputSentence.charAt(copy)))) {
-            for (int j = 0; j < k.length(); j++) {
-              if (inputSentence.charAt(copy) == k.charAt(j)) {
-                System.out.print(v);
-                break;
-              }
-              System.out.print(v);
-            }
-          }
-        });
-      }
+      if (inputText.charAt(i) != ' ')
+        System.out.print(str[inputText.charAt(i) - 'A']);
     }
   }
 }

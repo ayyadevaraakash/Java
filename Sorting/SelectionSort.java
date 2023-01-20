97067ad8b25ca9b0af9cf53c84b8f91aa3197d10
akash <akashudsouza12@gmail.com>
2023-01-20 17:09:05 +0530

    sorting practice

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,30 +1,32 @@
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        int length = arr.length;
+        for (int i = 0; i < length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < length; j++)
+                if (arr[j] < arr[smallest])
+                    smallest = j;
+            int temp = arr[i];
+            arr[i] = arr[smallest];
+            arr[smallest] = temp;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int capacity;
-        System.out.print("Enter number of elements: ");
-        capacity = sc.nextInt();
-        int[] myArray = new int[capacity];
+        System.out.print("Enter the capacity of the array: ");
+        int capacity = sc.nextInt();
+        int arr[] = new int[capacity];
         System.out.print("Enter the elements: ");
         for (int i = 0; i < capacity; i++)
-            myArray[i] = sc.nextInt();
-        // Logic
-        for (int i = 0; i < capacity - 1; i++) {
-            int temp, minimum = myArray[i], index = i;
-            for (int j = i + 1; j < capacity; j++) {
-                if (myArray[j] < minimum) {
-                    minimum = myArray[j];
-                    index = j;
-                }
-            }
-            temp = myArray[i];
-            myArray[i] = minimum;
-            myArray[index] = temp;
-        }
-        System.out.println("Sorted Array: " + Arrays.toString(myArray));
+            arr[i] = sc.nextInt();
+        System.out.println("Input Array: " + Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.print("Sorted Array: " + Arrays.toString(arr));
         sc.close();
     }
 }
